71d0e840ace833b757aa2684784906bbc8777b44
Sandy Johnson <sandythsu@gmail.com>
2020-04-26 18:49:08 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,9 +1,9 @@
-
 def turn_count(board)
   counter = 0
   board.each do |turn|
-    if turn == "X" || turn =="O"
-      counter += 1
+      if turn == 'X' || turn =="O"
+        counter += 1
+      end
     end
+    return counter
   end
-end
